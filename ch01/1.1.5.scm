32534a29616314738b37e5099885438c213a3fe9
sungsik.kim <skystar819@gmail.com>
2013-06-16 23:18:37 +0900

    add practice and problem1-1

---
+++ ch01/1.1.5.scm
@@ -0,0 +1,22 @@
+(define (abs x)
+ (cond ((> x 0) x)
+       ((= x 0) 0)
+       ((< x 0) (- x))))
+
+(define (abs x)
+ (cond ((< x 0) (- x))
+       (else x)))
+
+(define (abs x)
+ (if (< x 0) (- x)
+      x))
+
+(and (> x 5) (< x 10))
+
+(define (>= x y)
+ (or (> x y) (= x y)))
+
+(define (>= x y)
+ (not (< x y)))
+
+
