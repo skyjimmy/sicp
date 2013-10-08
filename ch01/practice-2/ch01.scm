933d5b03e4d62617c461c399131adf2281b6b874
sungsik.kim <skystar819@gmail.com>
2013-10-08 20:22:52 +0900

    complete ch1.1

--- ch01/practice-2/ch01.scm
+++ ch01/practice-2/ch01.scm
@@ -100,4 +100,29 @@ circumference ;; 62.8318
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
+;; 1.1.8
+(define (square x)
+  (exp (double (log x))))
+
+(define (double x) (+ x x))
+
+(define (sqrt x)
+  (define (good-enough? guess x)
+	(< (abs (- (square guess) x)) 0.001))
+  (define (improve guess x)
+	(average guess (/ x guess)))
+  (define (sqrt-iter guess x)
+	(if (good-enough? guess x) guess
+	  (sqrt-iter (improve guess x) x)))
+  (sqrt-iter 1.0 x))
+
+(define (sqrt x)
+  (define (good-enough guess)
+	(< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+	(average guess (/ x guess)))
+  (define (sqrt-iter guess)
+	(if (good-enough? guess x) guess
+	  (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
 
