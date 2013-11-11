1b6bec4488c37b03ff065eba39f104ace742237a
sungsik.kim <skystar819@gmail.com>
2013-11-11 22:16:48 +0900

    modify ch01.scm

--- ch01/practice-2/ch01.scm
+++ ch01/practice-2/ch01.scm
@@ -174,4 +174,23 @@ circumference ;; 62.8318
 		((= kinds-of-coins 4) 25)
 		((= kinds-of-coins 5) 50)))
 
+;; 1.2.4
+(define (expt b n)
+  (if (= n 0) 1
+	(* b (expt b (- n 1)))))
+
+(define (expt b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+  (if (= counter 0) product
+	(expt-iter b (- counter 1) (* b product))))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+		((even? n) (square (fast-expt b (/ n 2))))
+		(else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
 
