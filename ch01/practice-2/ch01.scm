fba546c4d6de440a977469f92ae35d5c53b184e1
sungsik.kim <skystar819@gmail.com>
2013-12-03 22:38:44 +0900

    add pro pro

--- ch01/practice-2/ch01.scm
+++ ch01/practice-2/ch01.scm
@@ -313,3 +313,52 @@ circumference ;; 62.8318
 	 (* y b)
 	 (* a b)))
 
+;; 1.3.3
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+	(if (close-enough? neg-point pos-point) midpoint
+	  (let ((test-value (f midpoint)))
+		(cond ((positive? test-value)
+			   (search f neg-point midpoint))
+			  ((negative? test-value)
+			   (search f midpoint pos-point))
+			  (else midpoint))))))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+		(b-value (f b)))
+	(cond ((and (negative? a-value) (positive? b-value))
+		   (search f a b))
+		  ((and (negative? b-value) (positive? a-value))
+		   (search f b a))
+		  (else
+			(error "Values are not of opposite sign" a b)))))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+	(< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+	(let ((next (f guess)))
+	  (if (close-enough? guess next)
+		next
+		(try next))))
+  (try first-guess))
+
+(define (sqrt-fixed-point x)
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
+
+;; 1.3.4
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (sqrt-pro-pro x)
+  (fixed-point (average-damp (lambda (y) (/ x y))) 1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y)))) 1.0))
+
