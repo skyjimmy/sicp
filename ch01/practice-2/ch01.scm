d37f5f8347cf1d89a83ee5dc5eb3ee182491a950
sungsik.kim <skystar819@gmail.com>
2013-12-07 11:44:11 +0900

    newtons

--- ch01/practice-2/ch01.scm
+++ ch01/practice-2/ch01.scm
@@ -362,3 +362,20 @@ circumference ;; 62.8318
 (define (cube-root x)
   (fixed-point (average-damp (lambda (y) (/ x (square y)))) 1.0))
 
+(define (deriv g)
+  (lambda (x)
+	(/ (- (g (+ x dx)) (g x)) dx)))
+
+(define dx 0.00001)
+
+(define (cube x) (* x x x))
+
+(define (newton-transform g)
+  (lambda (x)
+	(- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt-newton x)
+  (newtons-method (lambda (y) (- (square y) x)) 1.0))
