36dd19c8fb4486a246729610e2144105ae055d1a
sungsik.kim <skystar819@gmail.com>
2014-01-05 01:07:42 +0900

    solved 2-31

--- ch02/practice-2/ch02.scm
+++ ch02/practice-2/ch02.scm
@@ -428,4 +428,9 @@
 	   tree))
 
   
+;; problem 2-31
+(define (tree-map fn tree)
+  (cond ((null? tree) '())
+		((not (pair? tree)) (fn tree))
+		(else (cons (tree-map fn (car tree)) (tree-map fn (cdr tree))))))
 
