2a7d0a38024cb979020fe60c4b99a20c9ca9265e
sungsik.kim <skystar819@gmail.com>
2014-01-06 22:36:57 +0900

    add 2.2.3

--- ch02/practice-2/ch02.scm
+++ ch02/practice-2/ch02.scm
@@ -454,3 +454,61 @@
 	  (display rest)
 	  (newline)
 	  (append rest (map (lambda (x) (cons (car s) x)) rest)))))
+
+;; 2.2.3
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+		((not (pair? tree))
+		 (if (odd? tree) (square tree) 0))
+		(else (+ (sum-odd-squares (car tree))
+				 (sum-odd-squares (cdr tree))))))
+
+(define (fib n)
+  (cond ((= n 0) 0)
+		((= n 1) 1)
+		(else (+ (fib (- n 1)) (fib (- n 2))))))
+
+(define (even-fibs n)
+  (define (next k)
+	(if (> k n) '()
+	  (let ((f (fib k)))
+		(if (even? f)
+		  (cons  f (next (+ k 1)))
+		  (next (+ k 1))))))
+  (next 0))
+
+;; 차례열 연산
+(define (filter predicate sequence)
+  (cond ((null? sequence) '())
+		((predicate (car sequence))
+		 (cons (car sequence)
+			   (filter predicate (cdr sequence))))
+		(else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence) initial
+	(op (car sequence)
+		(accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high) '()
+	(cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) '())
+		((not (pair? tree)) (list tree))
+		(else (append (enumerate-tree (car tree))
+					  (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree)
+  (accumulate + 0 (map square (filter odd? (enumerate-tree tree)))))
+
+(define (even-fibs n)
+  (accumulate cons '() (filter even? (map fib (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate cons '() (map square (map fib (enumerate-interval 0 n)))))
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate * 1 (map square (filter odd? sequence))))
+
